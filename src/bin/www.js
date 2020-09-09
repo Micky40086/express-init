d606e440eadaaec8e272b7ef08d245d15edfdc1a
Mickey <Mickey@teamt5.org>
2020-09-09 18:33:36 +0800

    feat: add babel and other package

--- src/bin/www.js
+++ src/bin/www.js
@@ -4,15 +4,15 @@
  * Module dependencies.
  */
 
-var app = require('../app');
-var debug = require('debug')('mail-service:server');
-var http = require('http');
-
+import app from '../app';
+import debugLib from 'debug';
+import http from 'http';
+const debug = debugLib('mail-service:server');
 /**
  * Get port from environment and store in Express.
  */
 
-var port = normalizePort(process.env.PORT || '3000');
+var port = normalizePort(process.env.PORT || '3001');
 app.set('port', port);
 
 /**
---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+const router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify({ name: "Mickey" }));
+});
+
+export default router;
